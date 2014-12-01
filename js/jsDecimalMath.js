c7f9490544f27dfdf3978f2b12c5eb26d1be81a7
wboka <wboka@users.noreply.github.com>
2014-12-01 10:32:13 -0500

    Update jsDecimalMath.js

--- js/jsDecimalMath.js
+++ js/jsDecimalMath.js
@@ -1,23 +1,34 @@
+/*
+ *  jsDecimalMath - plugin by Wayne Boka (@wboka)
+ *  Documentation found at https://github.com/wboka/JSUtils/blob/master/js/jsDecimalMath.js
+ *  Version 1.0
+ */
 function jsDecimalMath(left, right, operand) {
+    // Get the numbers from the left and right of the decimal for each number passed in
     var leftNumberParts = left.toString().split(".");
     var rightNumberParts = right.toString().split(".");
     
+	// Find if they have any decimals
     var leftDecimalPlaces = leftNumberParts.length > 1 ? leftNumberParts[1].length : 0;
     var rightDecimalPlaces = rightNumberParts.length > 1 ? rightNumberParts[1].length : 0;
 
-    var largest = Math.max(leftDecimalPlaces, rightDecimalPlaces);
+	// Set the largest common factor
+	var largest = Math.max(leftDecimalPlaces, rightDecimalPlaces);
     var commonFactor = Math.pow(10, largest);
 	
+	// Multiply both numbers by the commonFactor
 	function makeInt(n) {
 		return n * commonFactor;
 	}
 	
+	// Divide both numbers by the commonFactor
 	function makeFloat(n) {
 		return n / commonFactor;
 	}
 	
 	var retValue = null;
 	
+	// Do math based on the operand passed in
 	switch (operand) {
 		case "+":
 			retValue = makeFloat(makeInt(left) + makeInt(right));
@@ -35,5 +46,6 @@ function jsDecimalMath(left, right, operand) {
 			retValue = "Error";
 	}
 	
+	// Return the correct value
 	return retValue;
 }
