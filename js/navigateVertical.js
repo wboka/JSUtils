1797c94c79e9e8d2856603195d3d3e49ad1fd12c
wboka <wboka@users.noreply.github.com>
2014-12-16 14:01:21 -0500

    Adds error messages
    - Throws an error if the elements are not in a td or tr in a table

--- js/navigateVertical.js
+++ js/navigateVertical.js
@@ -1,28 +1,43 @@
-// Function to navigate up and down a table
+/*
+ *  navigateVertical - plugin by Wayne Boka (@wboka)
+ *  Documentation found at https://github.com/wboka/JSUtils/blob/master/js/navigateVertical.js
+ *  Function to navigate up and down a table
+ *  Version 1.1.0
+ */
 function navigateVertical(e) {
 	var $this = $(this);
 	var $parentCell = $this.closest("td");
-	var $parentRow = $parentCell.closest("tr");
-	var thisIndex = $this.index();
-	var notSelect = !$this.is("select");
 	
-	if (notSelect && (e.which === 38 || e.which === 40)) {
-		if (e.which == 40 && rowCount - 1 > $parentRow.index()) {
-			$focusedRow = $parentRow.next("tr");
-		}
-		else if (e.which == 38 && $parentRow.index() > 0) {
-			$focusedRow = $parentRow.prev("tr");
-		}
+	if ($parentCell.length > 0) {
+		var $parentRow = $parentCell.closest("tr");
 		
-		if (typeof $focusedRow !== 'undefined') {
-			$focusedRow
-			.children()
-			.filter(":nth-child(" + ($parentCell.index() + 1) + ")")
-			.children()
-			.filter(":nth-child(" + (thisIndex + 1) + ")")
-			.focus()
-			.select();
+		if ($parentRow.length > 0) {
+			var thisIndex = $this.index();
+			var notSelect = !$this.is("select");
+			
+			if (notSelect && (e.which === 38 || e.which === 40)) {
+				if (e.which == 40 && rowCount - 1 > $parentRow.index()) {
+					$focusedRow = $parentRow.next("tr");
+				}
+				else if (e.which == 38 && $parentRow.index() > 0) {
+					$focusedRow = $parentRow.prev("tr");
+				}
+				
+				if (typeof $focusedRow !== 'undefined') {
+					$focusedRow
+					.children()
+					.filter(":nth-child(" + ($parentCell.index() + 1) + ")")
+					.children()
+					.filter(":nth-child(" + (thisIndex + 1) + ")")
+					.focus()
+					.select();
+				}
+				
+			}
+		} else {
+			throw new Error(2, "Navigation not called in a tr.");
 		}
-		
+	} else {
+		throw new Error(1, "Navigation not called in a td.");
 	}
 }
